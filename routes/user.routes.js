c650483c8370e2e6733de672125cd9de7692590f
Nitesh <niteshchandrakar2@gmail.com>
2023-10-30 19:15:52 +0530

    asr

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -27,7 +27,7 @@ res.status(200).json({msg:"Registraion Successfull"})
 UserRouter.post("/login",async(req,res)=>{
     try{
         const {email,password}=req.body
-        const user= await userModel.findOne({email:email})
+        const user= await userModel.findOne({email})
         if(user){
        const token=     jwt.sign({  email: user.email }, 'masai');
        bcrypt.compare(password, user.password, async(err, result)=> {
@@ -40,7 +40,7 @@ UserRouter.post("/login",async(req,res)=>{
     });
           
         }else{
-            res.status(400).json({msg:"Register First",user:user})
+            res.status(400).json({msg:"Register First",user:email})
         }
     }catch(error){
         res.status(400).json({msg:error.message})
